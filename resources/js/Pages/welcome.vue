cacf7ecf17deefa0d85138215cfe73ada9b43c96
Thawzin229 <thawzin842019@gmail.com>
2025-02-27 13:36:40 +0630

    project configuration

---
+++ resources/js/Pages/welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-2xl" v-if="visible">Hello {{name}}</div>
+</template>
+
+<script>
+export default {
+  data:()=>({
+    visible:false,
+    name:''
+  }),
+}
+</script>
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Events\TestingEvent;
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('welcome');
 });
